58a8befad300252ba8dbd2fd092dbb26596d429a
Joseph Jung <joseph.jung@icloud.com>
2021-04-08 11:38:28 -0600

    Initial Commit

---
+++ SingleStageRocketPerformanceCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SingleStageRocketPerformanceCalculator
+//
+//  Created by Joseph Jung on 4/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SingleStageRocketPerformanceCalculator/SingleStageRocketPerformanceCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SingleStageRocketPerformanceCalculatorApp.swift
+//  SingleStageRocketPerformanceCalculator
+//
+//  Created by Joseph Jung on 4/8/21.
+//
+
+import SwiftUI
+
+@main
+struct SingleStageRocketPerformanceCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
